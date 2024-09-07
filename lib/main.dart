0fa560cb3dbb08cedf1e7aac13513099175e2fbc
amshul <amshulmp@gmail.com>
2024-09-07 14:28:20 +0530

    release build

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       navigatorKey: ContextUtility.navigatorKey, 
       title: "deeplink",
       theme: ThemeData(),
